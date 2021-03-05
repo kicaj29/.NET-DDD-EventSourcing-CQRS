d621b4b5c0256b75bb03942317fa43c9eb25cebc
kicaj29 <kicaj29@wp.pl>
2021-03-05 14:20:27 +0100

    vet-clinic: added new project FrontDesk.SharedKernek

---
+++ vet-clinic/FrontDesk.SharedKernel/Enums/TrackingState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDesk.SharedKernel.Enums
+{
+    public enum TrackingState
+    {
+        Unchanged = 0,
+        Added = 1,
+        Modified = 2,
+        Deleted = 3
+    }
+}
