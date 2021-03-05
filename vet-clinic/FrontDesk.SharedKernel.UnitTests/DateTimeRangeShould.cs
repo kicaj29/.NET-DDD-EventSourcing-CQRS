93136f0f6bc66b8cd1641575ad1d445ecc9bb054
kicaj29 <kicaj29@wp.pl>
2021-03-05 15:10:51 +0100

    FrontDesk.SharedKernel.UnitTests.csproj

---
+++ vet-clinic/FrontDesk.SharedKernel.UnitTests/DateTimeRangeShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDesk.SharedKernel.UnitTests
+{
+    [TestFixture]
+    class DateTimeRangeShould
+    {
+        private DateTime _testStartDate = new DateTime(2014, 1, 1, 9, 0, 0);
+
+        [Test]
+        public void ThrowArgumentExceptionIfStartDateEqualsEndDate()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { new DateTimeRange(_testStartDate, _testStartDate); });
+        }
+    }
+}
